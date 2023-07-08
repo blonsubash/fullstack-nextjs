911be4bfba98ae82947f159ed0bc5806d0df9182
subashlama <subash@aricalot.com>
2023-07-08 18:19:42 +0545

    middle ware setup, protect private route, get token from db

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const router = useRouter();
+  const [data, setData] = useState("");
+  const logout = async () => {
+    try {
+      const res = await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Logout failed");
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/token");
+    console.log("res", res.data);
+    setData(res.data?.data._id);
+  };
+  return (
+    <div className="flex flex-col items-center">
+      ProfilePage
+      <h2>{data ? <Link href={`/profile/${data}`}>{data}</Link> : ""} </h2>
+      <hr />
+      <button
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white py-2 px-4 rounded"
+        onClick={logout}
+      >
+        Logout
+      </button>
+      <button className="bg-purple-500" onClick={getUserDetails}>
+        Hit me
+      </button>
+    </div>
+  );
 }
 
 export default ProfilePage;
